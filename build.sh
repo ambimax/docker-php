c99048d47bfe417c00d3e5c4c1a137711b5a0c97
Tobias Schifftner <ts@ambimax.de>
2018-08-15 16:25:16 +0200

    Updated docker credentials, added php 7.1

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ docker build --tag=builder --file=builder/Dockerfile --target builder .
 # PHP 7.0
 (cd 7.0/stretch/cli && docker build -t php7.0-cli-stretch .)
 (cd 7.0/stretch/fpm && docker build -t php7.0-fpm-stretch .)
+
+# PHP 7.1
+(cd 7.1/stretch/cli && docker build -t php7.1-cli-stretch .)
+(cd 7.1/stretch/fpm && docker build -t php7.1-fpm-stretch .)

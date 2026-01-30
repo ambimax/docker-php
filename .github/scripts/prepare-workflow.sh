497cd904030bcf61e6494475c1be9a1783ef72b6
eckhardtmarie <me@quokka-commerce.de>
2026-01-30 14:05:54 +0100

    fix

--- .github/scripts/prepare-workflow.sh
+++ .github/scripts/prepare-workflow.sh
@@ -13,7 +13,7 @@ wget -qO- 'https://github.com/tianon/pgp-happy-eyeballs/raw/master/hack-my-build
 
 # Install orca.phar
 curl -O https://orca-build.io/downloads/orca.zip \
-    && unzip -o orca.zip
+    && unzip -o orca.zip \
     && chmod +x orca.phar
 
 # Install prettier

9c88d0c7185fa8903780dcd3a363e69e07846a77
marieeckhardt <me@ambimax.de>
2026-05-04 13:39:45 +0200

    add workflow file

--- .github/scripts/prepare-workflow.sh
+++ .github/scripts/prepare-workflow.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-#ARCH=$([ "$RUNNER_ARCH" = "ARM64" ] && echo "arm64" || echo "amd64")
+ARCH=$([ "$RUNNER_ARCH" = "ARM64" ] && echo "arm64" || echo "amd64")
 
 #Install container structure test
-#curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-"${ARCH}" \
-#    && chmod +x container-structure-test-linux-"${ARCH}" \
-#    && sudo mv container-structure-test-linux-"${ARCH}" /usr/local/bin/container-structure-test
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-"${ARCH}" \
+    && chmod +x container-structure-test-linux-"${ARCH}" \
+    && sudo mv container-structure-test-linux-"${ARCH}" /usr/local/bin/container-structure-test
 
 # Clone official docker repo for docker test suite
-#git clone https://github.com/docker-library/official-images.git ~/official-images
+git clone https://github.com/docker-library/official-images.git ~/official-images
 
 # Add "pgp-happy-eyeballs" to help cut down on gpg-related issues
 wget -qO- 'https://github.com/tianon/pgp-happy-eyeballs/raw/master/hack-my-builds.sh' | bash
